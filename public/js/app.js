0c00c05b0019f404edc34b02b4d7e045ac48e5eb
Denes Kelenyi <denes.kelenyi@me.com>
2013-04-11 17:33:36 -0600

    Issue #7 state is showing undefined when action is ...

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,13 @@
-var countyListArray = new Array();
-counter = 0;
-var newRow = "";
-var key = "";
-counter = 0;
-var states = new Array("Active", "Pending", "Completed", "Inactive", "Unknown");
-$.getJSON("data/channels.json", function(counties) {
-    channelList = counties;
-    color = "white";
+var countyListArray = new Array(); // the array to hold county list
+counter = 0; 
+var newRow = ""; // row to be inserted
+var key = ""; //this is the index in the county array
+var states = new Array("Active", "Pending", "Completed", "Inactive", "Unknown"); //this is the string value for states 
+$.getJSON("data/channels.json", function(counties) { ///let's make a json request
+    //channelList = counties; // and put it in a chanelList -- and then never use it again :)
+    color = "white"; //default color for display text
     for (var key in counties) { 
-        if (key != 0) {
+        if (key != 0) { // we don't process "All" in county list 0=> All
             countyName = counties[key];
             console.log("subscribed to:", key)
             countyListArray[key] = bio.join(key);
@@ -24,16 +23,8 @@ $.getJSON("data/channels.json", function(counties) {
                     color = "green";
                 }
                 counter++;
-                if (action == "error") {
-                    newRow = "<tr id=\"tr" + counter + "\">";
-                    newRow += "<td><font color='" + color + "'>" + action + "&nbsp;</color></td>";
-                    var errorString="";
-                    if(typeof data.description !== "undefined") {
-                        errorString=data.description;
-                    }
-                    newRow += "<td  colspan=\"7\">" +errorString  + "&nbsp;</td>";
-                    newRow += "</tr>";
-                } else {
+                
+                if (action !== "error") {
                     try {
                         owner_name = data.owner.name;
                         owner_id = data.owner.id;
@@ -44,43 +35,38 @@ $.getJSON("data/channels.json", function(counties) {
                     newRow = "<tr id=\"tr" + counter + "\">";
                     newRow += "<td><font color='" + color + "'>" + action + "&nbsp;</color></td>";
                     newRow += "<td>" + counties[data.county] + "&nbsp;</td>";
-                    //newRow+="<td>"+countyName+"</td>";
-                    //newRow+="<td>"+key+"&nbsp;</td>"; 
-                    //newRow+="<td>"+data.id+"&nbsp;</td>"; 
-                    //newRow+="<td>"+data.description+"&nbsp;</td>"; // apparently this data doesn't exist
                     var stateString="";
-                    if(typeof data.state !=="undefined") {
-                        stateString=states[data.state];
+                    if(typeof data.state !== "undefined") {
+                        //protect state from showing undefined
+                        stateString = states[data.state];
+                    
                     }
                     newRow += "<td>" + stateString + "&nbsp;</td>";
                     newRow += "<td>" + owner_name + "&nbsp;</td>";
                     newRow += "<td>" + owner_id + "</td>";
                     newRow += "</tr>";
-                }
-
-
                 $('#actionList tr:first').after(newRow);
-                $('#tr' + counter + '').effect("highlight", {color: color}, 2000);
+                $('#tr' + counter + '').effect("highlight", {color: color}, 2000); 
+                }
             });
-
-
-
         }//if key not 0
-    }// freaking long foreach on the bids objec (counties in reality) ;)
-
+    }// freaking long foreach on the bids object (counties in reality) ;)
 });//json init --- async !!!!
-
+/*
+ * make connection to the remote
+ */
 bio = bio(io, 'http://' + window.location.hostname + ':3000');
 var socket = bio.connect();
 socket.on('connect', function() {
-    $('#connection').html('<font color=green>connected</font>');
+    //we removed it as it wasn't needed I leave it here for reference
+    //$('#connection').html('<font color=green>connected</font>');
 });
-
-Array.prototype.remove = function(from, to) {
+/*
+ * Remove range from array
+ */
+Array.prototype.remove = function(from, to) { 
     var rest = this.slice((to || from) + 1 || this.length);
     this.length = from < 0 ? this.length + from : from;
     return this.push.apply(this, rest);
 };
-function keyPress() {
-    //not at this time :O
-}
+//end
